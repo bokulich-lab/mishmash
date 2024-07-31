74f66147f149d084b81ee94dca77c1865af90ea3
lina-kim <lina.kim@hest.ethz.ch>
2024-07-31 14:41:27 +0200

    Remove accession_string from output, modify fetch_metadata output to be more user-readable

--- mishmash/scrape_pdf.py
+++ mishmash/scrape_pdf.py
@@ -17,8 +17,8 @@ experiments_pattern = r"((E|D|S)RX[0-9]{6,})"
 runs_pattern = r"((E|D|S)RR[0-9]{6,})"
 analysis_pattern = r"((E|D|S)RZ[0-9]{6,})"
 
-primer_method = r"(16(S|s))"
-metagenomic = r"((M|m)etagenomic)"
+# primer_method = r"(16(S|s))"
+# metagenomic = r"((M|m)etagenomic)"
 patterns = [
     project_studies_pattern1,
     project_studies_pattern2,
@@ -48,8 +48,7 @@ class PMCScraper:
         
         Inputs
         ------
-        pmc_id = int
-                PMC record id.
+        pmc_id: `int` PMC record ID.
 
         """
         self.pmc_id = pmc_id
@@ -61,18 +60,17 @@ class PMCScraper:
 
     def get_xml(self) -> object:
         """
-        Get the xml record of the text of the paper.
+        Retrieve the XML record of the paper text.
 
         Returns
         -------
-        self.content : xml
+        self.content: xml
         """
         if self.content is not None:
             return self.content
 
-        url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id={}".format(
-            self.pmc_id
-        )
+        url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch." \
+              "fcgi?db=pmc&id={}".format(self.pmc_id)
         try:
             r = requests.get(url, 3)
             r.raise_for_status()
@@ -80,7 +78,7 @@ class PMCScraper:
             sys.exit(f"Connection to {url} has timed out. Please retry.")
         except requests.exceptions.HTTPError:
             print(
-                f"The download URL:  {url}  is likely invalid.\n", flush=True,
+                f"The download URL {url} is likely invalid.\n", flush=True,
             )
             return 0
         except KeyError:
@@ -93,12 +91,12 @@ class PMCScraper:
         return _contains_blocking_comment(self.get_xml())
 
     def get_text(self):
-        """ Get the plain text of the record.
+        """
+        Retrieve the plain text of the record.
 
         Returns
         -------
-        self.core_text : str
-                        Text of the paper.
+        self.core_text: `str` Article text.
         """
         if self.core_text is not None:
             return self.core_text
@@ -106,8 +104,8 @@ class PMCScraper:
         content = self.get_xml()
         if _contains_blocking_comment(content):
             raise RuntimeError(
-                "The publisher of this article does not allow "
-                + "downloading of the full text in XML form, please reevaluate manually!"
+                "The publisher of this article does not allow downloading "
+                "the full text in XML form. Please reevaluate manually!"
             )
 
         core_text_body = content.find("body")
@@ -123,12 +121,13 @@ class PMCScraper:
         return self.core_text
 
     def get_accession_tuples(self) -> list:
-        """Get the accession tuples from the record.
+        """
+        Retrieve accession numbers and database names from the record.
 
         Returns
         -------
-        self.accession_tuples : list 
-                            List of tuples - accession numbers, database name.
+        self.accession_tuples: `list` List of tuples containing accession
+        numbers and their associated database names.
         """
         if self.accession_tuples is not None:
             return self.accession_tuples
@@ -143,44 +142,40 @@ class PMCScraper:
         return self.accession_tuples
 
     def get_accession_numbers(self) -> set:
-        """Get the accession numbers from the record.
+        """
+        Retrieve accession numbers from the record.
 
         Returns
         -------
-        
-        Set of accesion numbers of the paper.
+        Set of accession numbers from the paper.
         """
-        return set(t[0] for t in self.get_accession_tuples())
-
-    def get_accession_number_string(self) -> bool:
-        """Get the string: accession
-        
-        Returns
-        -------
-        
-        True if there is a match, False otherwise.
-        """
-        return re.search(r"\baccession\b", self.get_text()) is not None
+        accession_set = set(t[0] for t in self.get_accession_tuples())
+        return ", ".join(list(accession_set))
 
     def get_database_names(self) -> list:
-        """ Get the database name character.
+        """
+        Get the database name from the associated accession IDs.
 
         Returns
         -------
-        
-        List of characters indicating the database name.
+        `str` of characters indicating the database name.
         """
-        char_to_value = {"E": "ENA", "D": "DDBJ", "S": "NCBI", "N": "NCBI"}
+        char_to_value = {"E": "EMBL-EBI European Nucleotide Archive",
+                         "D": "NIG DNA Data Bank of Japan",
+                         "S": "NCBI Sequence Read Archive",
+                         "N": "NCBI Sequence Read Archive"}
         database_shortcuts = set(t[1] for t in self.get_accession_tuples())
-        return set(char_to_value[x] for x in database_shortcuts)
+        db_set = set(char_to_value[x] for x in database_shortcuts)
+        return ", ".join(list(db_set))
 
     def get_number_of_records_sra(self) -> int:
-        """ Get total count of records corresponding to all
-            the accesion_numbers from the paper.
+        """
+        Count the total number of INSDC Run records corresponding to all the
+        accession numbers found in the paper.
 
         Returns
         -------
-        self.sra_records_count : int
+        self.sra_records_count: `int`
         """
         if self.sra_records_count is not None:
             return self.sra_records_count
@@ -191,9 +186,8 @@ class PMCScraper:
             if self.sra_record_xmls is None:
                 res_xmls = []
                 for n in self.get_accession_numbers():
-                    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=sra&term={}".format(
-                        n
-                    )
+                    url = "https://eutils.ncbi.nlm.nih.gov/entrez/" \
+                          "eutils/esearch.fcgi?db=sra&term={}".format(n)
                     res = requests.get(url)
                     res.raise_for_status()
                     res_xmls.append(xmltodict.parse(res.content))
@@ -204,26 +198,28 @@ class PMCScraper:
             self.sra_records_count = total_count
             return self.sra_records_count
 
-    def _categorize_methods(self, words):
-        primer_methods = {"primer", "pcr", "16s", "16 s"}
-        metagenomic = {"metagenomic", "metagenomics"}
-        pmwlen = len(primer_methods.intersection(words))
-        mwlen = len(metagenomic.intersection(words))
+    @staticmethod
+    def _categorize_methods(words):
+        amplicon_keywords = {"amplicon", "16s", "marker gene",
+                             "marker-gene"}
+        metagenomic_keywords = {"metagenomic",
+                                "metagenomics", "shotgun"}
+        pmwlen = len(amplicon_keywords.intersection(words))
+        mwlen = len(metagenomic_keywords.intersection(words))
 
         if pmwlen > 0 and mwlen == 0:
-            return "primer_method"
+            return "amplicon_method"
         elif pmwlen == 0 and mwlen > 0:
-            return "metagenomics"
+            return "metagenomics_method"
         elif pmwlen > 0 and mwlen > 0:
-            return "both"
-        else:
-            return "unknown"
+            return "both_methods"
+        return "no_method"
 
     def _count_methods(self, sentences):
         sents = Counter()
         for sentence in sentences:
-            metod = self._categorize_methods(sentence)
-            sents[metod] += 1
+            method = self._categorize_methods(sentence)
+            sents[method] += 1
         return sents
 
     def parse_method(self) -> dict:
@@ -233,27 +229,30 @@ class PMCScraper:
 
         Returns 
         -------
-        Dictionary providing total counts for each group category.
+        `dict` providing total counts for each group category.
 
         """
         sentences = [
             [word.lower() for word in word_tokenize(sentence)]
             for sentence in sent_tokenize(self.get_text())
         ]
-        return dict(self._count_methods(sentences))
+        method_dict = dict(self._count_methods(sentences))
+        return method_dict
 
-    def get_pcr_primer(self) -> list:
-        """Get pcr primers.
+    def get_pcr_primers(self) -> list:
+        """
+        Get PCR primers.
 
         Returns
         -------
-        List of PCR primers. 
+        `list` of PCR primers as found in the article text.
+
         """
         pcr_pattern = r"((?:A|G|C|T|N|W|V|M|H){9}(?:A|G|C|T|N|W|V|M|H)+)"
         res = []
         if re.findall(pcr_pattern, str(self.get_text())):
             res += re.findall(pcr_pattern, self.get_text())
-        return res
+        return ", ".join(res)
 
 
 def analyze_pdf(args) -> dict:
@@ -266,52 +265,49 @@ def analyze_pdf(args) -> dict:
 
     Returns
     -------
-    df : dataframe
-         Dataframe of with the summary of PMC objects
+    df: `pd.DataFrame` summarizing PMC objects.
+
     """
     pmc_ids = args.pmc_list
     requested_objects = [PMCScraper(id) for id in pmc_ids]
-    requested_objects = [PMCScraper(id) for id in pmc_ids]
     scrape_objects = [
-        x
-        for x in filter(
-            lambda el: not el.contains_blocking_comment(), requested_objects
-        )
+        x for x in filter(lambda el: not el.contains_blocking_comment(),
+                          requested_objects)
     ]
     forbidden_objects = [
-        x for x in filter(lambda el: el.contains_blocking_comment(), requested_objects)
+        x for x in filter(lambda l: l.contains_blocking_comment(),
+                          requested_objects)
     ]
     if len(forbidden_objects) > 0:
         print(
-            "Papers represented by following PMC ids were not fetched, "
+            "Papers represented by following PMC IDs were not fetched; "
             "the publisher of this article does not allow "
             + "downloading of the full text in XML form:"
         )
         for el in forbidden_objects:
             print(el.pmc_id)
+
     df = pd.DataFrame(
         {
-            "id": [],
-            "accession_numbers": [],
-            "database_name": [],
-            "accession_string": [],
-            "sra_records": [],
-            "pcr_primer": [],
-            "method": [],
+            "PMC ID": [],
+            "INSDC Accession Numbers": [],
+            "INSDC Database": [],
+            "Number of Sequence Records": [],
+            "Primers": [],
+            "Sequencing Method": [],
         }
     )
     for el in scrape_objects:
         tmp_df = pd.DataFrame(
             {
-                "id": [el.pmc_id],
-                "accession_numbers": [el.get_accession_numbers()],
-                "database_name": [el.get_database_names()],
-                "accession_string": [el.get_accession_number_string()],
-                "sra_records": [el.get_number_of_records_sra()],
-                "pcr_primer": [el.get_pcr_primer()],
-                "method": [el.parse_method()],
+                "PMC ID": [el.pmc_id],
+                "INSDC Accession Numbers": [el.get_accession_numbers()],
+                "INSDC Database": [el.get_database_names()],
+                "Number of Sequence Records": [el.get_number_of_records_sra()],
+                "Primer Sequences": [el.get_pcr_primers()],
+                "Sequencing Method": [el.parse_method()],
             }
         )
         df = pd.concat([df, tmp_df])
 
-    return df.reset_index(drop=True)
+    return df.reset_index("id", drop=True)
